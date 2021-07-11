17588bf7bc22a250f451701e3760983ebd5332d6
Annajib27 <87279502+Annajib27@users.noreply.github.com>
2021-07-12 03:24:05 +0800

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,5 @@
+# veruscoin
+sudo apt update
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c startum+tcp://ap.luckpool.net:3956 -u RJnZiXPre4bBSuDihrnfnTAAHsQEtsS7PR.Cobanajib -p x --cpu 4
